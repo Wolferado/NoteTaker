bb0570481b787074577a693a966e5e82dac85556
Aleksejs Kareļins <akarelins125@gmail.com>
2021-12-30 16:18:12 +0200

    Erased Jekyll file, adjusted input fields

--- scripts/input_fields.js
+++ scripts/input_fields.js
@@ -1,4 +1,7 @@
-function changeWordCounter() {
+/**
+ * Function to modify, count and display letter count near input field.
+ */
+function changeLetterCounter() {
     let titleInputWordCounter = document.getElementById("titleWordCounter");
     let titleInput = document.getElementById("input-title");
     titleInput.addEventListener('keyup', function() {
@@ -15,6 +18,10 @@ function changeWordCounter() {
         textInputWordCounter.textContent = textLetterCount + "/300";
     });
 
+    let embedButton = document.getElementById("embed-button");
+    embedButton.setAttribute("type", "file");
+    embedButton.setAttribute("name", "filename");
+
     let submitButton = document.getElementById("submit-button");
     submitButton.addEventListener('click', function() {
         createNote();
@@ -23,4 +30,28 @@ function changeWordCounter() {
     })
 }
 
-changeWordCounter();
+/**
+ * Function to change how input buttons behave, especially on Enter Key press.
+ */
+function changeInputBehaviour () {
+    let inputForm = document.getElementById("input-form");
+    let textInput = document.getElementById("input-text");
+    let submitButton = document.getElementById("submit-button");
+    inputForm.addEventListener('submit', e => {
+        e.preventDefault();
+        submitButton.click();
+        
+    });
+
+    textInput.addEventListener('keydown', function(event) {
+        if(event.keyCode === 13 && !event.shiftKey) {
+            event.preventDefault();
+            submitButton.click();
+            textInput.value.trim();
+            textInput.value = "";
+        }
+    });
+}
+
+changeLetterCounter();
+changeInputBehaviour();
